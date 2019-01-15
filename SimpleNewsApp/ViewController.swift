b8da26e5a73d57937153aea1783ebf63531dcc19
Rehan meo <rehanmeo@outlook.com>
2019-01-15 18:02:40 +0500

    Initial Commit

---
+++ SimpleNewsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleNewsApp
+//
+//  Created by M.Rehan on 2019-01-15.
+//  Copyright © 2019 M.Rehan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
