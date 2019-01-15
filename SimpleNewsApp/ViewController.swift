d529c182e99b1070114acf21560ba8e62035b7a6
Rehan meo <rehanmeo@outlook.com>
2019-01-15 20:06:17 +0500

    label add by Rehan

--- SimpleNewsApp/ViewController.swift
+++ SimpleNewsApp/ViewController.swift
@@ -10,9 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var label: UILabel = {
+        let l : UILabel = UILabel()
+        l.text = "My name is Muhammad Rehan"
+        l.textColor = UIColor .red
+        l.translatesAutoresizingMaskIntoConstraints = false
+        return l
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.view.addSubview(label)
+        label.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        label.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
         
     }
 }
